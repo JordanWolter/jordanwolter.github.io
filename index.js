57501c9213a24beee9f07a051d36eddd5aa7f838
JordanWolter <107160326+JordanWolter@users.noreply.github.com>
2022-06-27 17:14:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,7 @@ var i;
 
 for (i = 0; i < acc.length; i++) 
 {
-  acc[i].addEventListener("click");
-  function()
-  {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+  
     if (panel.style.display === "block") 
     {
       panel.style.display = "none";
