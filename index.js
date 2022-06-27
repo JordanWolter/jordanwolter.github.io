5c7579893b7c3c724b78a7ac45e4ed50193df870
JordanWolter <107160326+JordanWolter@users.noreply.github.com>
2022-06-27 16:02:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-function openPage(pageName, elmnt, color) {
+function openPage(pageName, elmnt, color) 
+{
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
+  for (i = 0; i < tabcontent.length; i++) 
+  {
     tabcontent[i].style.display = "none";
   }
 
   tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
+  for (i = 0; i < tablinks.length; i++) 
+  {
     tablinks[i].style.backgroundColor = "";
   }
 
@@ -17,28 +20,36 @@ function openPage(pageName, elmnt, color) {
 var acc = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+for (i = 0; i < acc.length; i++) 
+{
+  acc[i].addEventListener("click", function()
+  {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
+    if (panel.style.display === "block") 
+    {
       panel.style.display = "none";
-    } else {
+    } 
+    else 
+    {
       panel.style.display = "block";
     }
-  });
+  }
 }
 
-function openSchool(evt, schoolName) {
+function openSchool(evt, schoolName) 
+{
   var i, tabschool, tabstuff
  
   tabschool = document.getElementsByClassName("tabschool");
-  for (i = 0; i < tabschool.length; i++) {
+  for (i = 0; i < tabschool.length; i++) 
+  {
     tabschool[i].style.display = "none";
   }
 
   tabstuff = document.getElementsByClassName("tabstuff");
-  for (i = 0; i < tabstuff.length; i++) {
+  for (i = 0; i < tabstuff.length; i++) 
+  {
     tabstuff[i].className = tabstuff[i].className.replace("active", "");
   }
 
