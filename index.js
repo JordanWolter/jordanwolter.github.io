352158c4633484394bb2cceb32498e38f024bb33
JordanWolter <107160326+JordanWolter@users.noreply.github.com>
2022-06-27 18:10:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ for (i=0; i<accordion.length; i++)
 {
   accordion[i].addEventListener("click", function () {
     this.classList.toggle("active")
-  })
+  });
 }
 
 function openSchool(evt, schoolName) 
