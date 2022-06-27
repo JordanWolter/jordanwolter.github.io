1f11b697fcd8325e1a33dfeb28254f5ff0cd546b
JordanWolter <107160326+JordanWolter@users.noreply.github.com>
2022-06-27 16:26:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) 
-{
-  acc[i].addEventListener("click", function()
-  {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") 
-    {
-      panel.style.display = "none";
-    } 
-    else 
-    {
-      panel.style.display = "block";
-    }
-  });
-}
-
 function openPage(pageName, elmnt, color) 
 {
   var i, tabcontent, tablinks;
@@ -37,8 +17,25 @@ function openPage(pageName, elmnt, color)
   elmnt.style.backgroundColor = color;
 }
 
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-
+for (i = 0; i < acc.length; i++) 
+{
+  acc[i].addEventListener("click", function()
+  {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display == "block") 
+    {
+      panel.style.display = "none";
+    } 
+    else 
+    {
+      panel.style.display = "block";
+    }
+  });
+}
 
 function openSchool(evt, schoolName) 
 {
