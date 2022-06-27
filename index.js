6a4d8f68a349111c1878f1059a57896a1eaeb476
JordanWolter <107160326+JordanWolter@users.noreply.github.com>
2022-06-27 18:01:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function openPage(pageName, elmnt, color)
 const accordion = document.getElementsByClassName("container");
 
 for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active")
   })
 }
 
