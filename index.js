e4aa358d8bde45319cd1353373b57f169ebfceb6
JordanWolter <107160326+JordanWolter@users.noreply.github.com>
2022-06-27 16:24:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-function openPage(pageName, elmnt, color) 
-{
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) 
-  {
-    tabcontent[i].style.display = "none";
-  }
-
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) 
-  {
-    tablinks[i].style.backgroundColor = "";
-  }
-
-  document.getElementById(pageName).style.display = "block";
-  elmnt.style.backgroundColor = color;
-}
-function openWork()
-{
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -37,7 +17,28 @@ for (i = 0; i < acc.length; i++)
     }
   });
 }
+
+function openPage(pageName, elmnt, color) 
+{
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) 
+  {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) 
+  {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
 }
+
+
+
 
 function openSchool(evt, schoolName) 
 {
