a385ca632d232c7bb02235a081e803b2ba6b6fc0
JordanWolter <107160326+JordanWolter@users.noreply.github.com>
2022-06-27 20:22:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,94 +17,40 @@ function openPage(pageName, elmnt, color)
   elmnt.style.backgroundColor = color;
 }
 
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) 
+
+function openSchool(evt, schoolName) 
 {
-  acc[i].addEventListener("click", function() 
-                          {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") 
-    {
-      panel.style.display = "none";
-    } else
-    {
-      panel.style.display = "block";
-    }
-  });
-}
-
-function openSchool(evt, schoolName) {
   var i, tabschool, tabstuff
  
   tabschool = document.getElementsByClassName("tabschool");
-  for (i = 0; i < tabschool.length; i++) {
+  for (i = 0; i < tabschool.length; i++) 
+  {
     tabschool[i].style.display = "none";
   }
 
   tabstuff = document.getElementsByClassName("tabstuff");
-  for (i = 0; i < tabstuff.length; i++) {
-    tabstuff[i].className = tabstuff[i].className.replace("active");
+  for (i = 0; i < tabstuff.length; i++) 
+  {
+    tabstuff[i].className = tabstuff[i].className.replace("active", "");
   }
 
   document.getElementById(schoolName).style.display = "block";
   evt.currentTarget.className += "active";
+}
 
-// function openPage(pageName, elmnt, color) 
-// {
-//   var i, tabcontent, tablinks;
-//   tabcontent = document.getElementsByClassName("tabcontent");
-//   for (i = 0; i < tabcontent.length; i++) 
-//   {
-//     tabcontent[i].style.display = "none";
-//   }
-
-//   tablinks = document.getElementsByClassName("tablink");
-//   for (i = 0; i < tablinks.length; i++) 
-//   {
-//     tablinks[i].style.backgroundColor = "";
-//   }
-
-//   document.getElementById(pageName).style.display = "block";
-//   elmnt.style.backgroundColor = color;
-// }
-
-
-
-// function openSchool(evt, schoolName) 
-// {
-//   var i, tabschool, tabstuff
- 
-//   tabschool = document.getElementsByClassName("tabschool");
-//   for (i = 0; i < tabschool.length; i++) 
-//   {
-//     tabschool[i].style.display = "none";
-//   }
-
-//   tabstuff = document.getElementsByClassName("tabstuff");
-//   for (i = 0; i < tabstuff.length; i++) 
-//   {
-//     tabstuff[i].className = tabstuff[i].className.replace("active", "");
-//   }
-
-//   document.getElementById(schoolName).style.display = "block";
-//   evt.currentTarget.className += "active";
-// }
-
-// const accordionBtns = document.querySelectorAll(“accordion”);
-// accordionBtns.forEach((accordion) => {
-//   accordion.onclick = function () {
+const accordionBtns = document.querySelectorAll(“accordion”);
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
     
-//     this.classList.toggle(“is-open”);
-//     let content = this.nextElementSibling;
-//     if (content.style.maxHeight) {
-//       //this is if the accordion is open
-//       content.style.maxHeight = null;
-//     } else {
-//       //if the accordion is currently closed
-//       content.style.maxHeight = content.scrollHeight + “px”;
-//     }
-//   };
-// });
+    this.classList.toggle(“is-open”);
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + “px”;
+    }
+  };
+});
